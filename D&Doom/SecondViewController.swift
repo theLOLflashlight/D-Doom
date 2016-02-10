d4e9bc7b1060cae0c82796a38cb66663caa2adc6
Andrew Meckling <a00854442@btechgames-imac-25.wifi.bcit.ca>
2016-02-10 12:32:12 -0800

    Initial Commit

---
+++ D&Doom/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  D&Doom
+//
+//  Created by Andrew Meckling on 2016-02-10.
+//  Copyright © 2016 Andrew Meckling. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
