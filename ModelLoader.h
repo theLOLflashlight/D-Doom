b402739ddf52b8ba17749b21f5059271a1150eaa
theLOLflashlight <andrew.t.meckling@gmail.com>
2016-03-22 16:49:05 -0700

    Added files via upload
    Visual Studio doesn't want to upload :(

---
+++ ModelLoader.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "GLHandle.h"
+#include "Material.h"
+#include "Vertex.h"
+
+
+#include <vector>
+#include <string>
+
+
+struct TextureMap
+{
+    std::shared_ptr< Material > material;
+    int offset, count;
+
+    TextureMap() = default;
+
+    TextureMap( std::shared_ptr< Material > mtl, int offset )
+        : material( mtl )
+        , offset( offset )
+        , count( 0 )
+    {
+    }
+
+    void finish( int end )
+    {
+        count = end - offset;
+    }
+};
+
+struct ObjMesh
+    : public std::vector< ObjVertex >
+{
+    std::vector< TextureMap > textures;
+
+    explicit ObjMesh( std::string path );
+};
+
+
